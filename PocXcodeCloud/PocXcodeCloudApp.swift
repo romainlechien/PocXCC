3d492a8cab7784c5cc1253d10c89e64652596db8
LECHIEN Romain (i-BP - CELAD) <romain.lechien_ssii@-bp.fr>
2022-02-10 15:05:14 +0100

    Initial Commit

---
+++ PocXcodeCloud/PocXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocXcodeCloudApp.swift
+//  PocXcodeCloud
+//
+//  Created by Romain LECHIEN on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PocXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
